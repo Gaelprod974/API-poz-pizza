993e6d98075348804167382a41ecd435973f2693
Gaelprod974 <gaelprod974@gmail.com>
2025-03-19 00:12:23 +0400

    vercel2

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Connexion à MongoDB avec gestion des erreurs
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('✅ Connecté à MongoDB'))
     .catch(err => {
         console.error('❌ Erreur MongoDB :', err);
